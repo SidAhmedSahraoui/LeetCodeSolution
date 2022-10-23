cedaeafabdfef99fbfc3f9e8565652217b220cbf
Sid Ahmed <s.sahraoui@esi-sba.dz>
2022-10-23 10:43:24 +0100

    Runtime 2856 ms Beats 5.17% Memory 45 MB Beats82.43%

--- 001-set-mismatch.js
+++ 001-set-mismatch.js
@@ -2,21 +2,22 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var findErrorNums = function(nums) {
-    let d,m,r = 0
-    for (let i = 0; i < nums.length; i++) {
-            if(nums.find(el => el === i+1) === undefined) {
-                     m = i+1;
-                }
-            for (let j = 0; j < nums.length; j++) {
-                if(i !== j){
-                     if(nums[j] === nums[i]){
-                        d = nums[j];
-                        break;
-                    }
-                }
-                
-            }
+var findErrorNums = function (nums) {
+  let d,
+    m,
+    r = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.find((el) => el === i + 1) === undefined) {
+      m = i + 1;
     }
-    return [d,m]
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j) {
+        if (nums[j] === nums[i]) {
+          d = nums[j];
+          break;
+        }
+      }
+    }
+  }
+  return [d, m];
 };
